899a73f23c68b7a81c609fd0a46ce4fd777d9456
unknown <ianchen6501@gmail.com>
2020-06-28 21:23:01 +0800

    hw4.5.6

--- homeworks/week2/hw4.js
+++ homeworks/week2/hw4.js
@@ -1,5 +1,12 @@
 function printFactor(n) {
-  
+    var factor = []
+    for(var i = 1; i<=n ;i++ ){
+        if(n%i === 0)
+            factor.push(i)
+    }
+    for(var i = 0; i<factor.length ;i++){
+        console.log(factor[i])
+    }
 }
 
-printFactor(10);
+printFactor(60);
--- homeworks/week2/hw5.js
+++ homeworks/week2/hw5.js
@@ -1,10 +1,19 @@
 function join(arr, concatStr) {
-  
+    var str = ""
+    for(var i = 1 ;i <arr.length ;i++){
+        var str = str + (concatStr + arr[i])
+    }
+    return arr[0]+str
 }
 
 function repeat(str, times) {
-  
-}
+    var result = ""
+    for(i=0 ; i<times ; i++){
+    var result = result + str    
+    }
+    return result
+  }
+
 
 console.log(join(['a'], '!'));
 console.log(repeat('a', 5));
