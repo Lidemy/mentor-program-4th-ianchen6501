3b03283b4c106a8f6e62123e690f86c785902b25
unknown <ianchen6501@gmail.com>
2020-07-05 14:47:30 +0800

    hw1

--- homeworks/week3/hw1.js
+++ homeworks/week3/hw1.js
@@ -1,23 +1,21 @@
-var readline = require('readline');
-var rl = readline.createInterface({
-  input: process.stdin
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
 });
-
-var lines = []
-
-rl.on('line', function (line) {
-  lines.push(line)
+// 設定 readline 標準輸入
+const lines = [];
+rl.on('line', (line) => {
+  lines.push(line);
 });
-
-rl.on('close', function() {
-  solve(lines)
-})
-  
-function solve(lines){
-  	var n = Number(lines[0])
-    var str = ''
-    for(i=0 ; i < n ; i++){
-        var str = str + '*'
-        console.log(str)
-    }
+function solve(n) {
+  const result = [];
+  for (let i = 1; i <= n; i += 1) {
+    result.push('*');
+    console.log(result.join(''));
+  }
 }
+rl.on('close', () => {
+  solve(lines);
+});
+// 輸入結束，arr 裡面已經有輸入的資料，並執行 solve function
