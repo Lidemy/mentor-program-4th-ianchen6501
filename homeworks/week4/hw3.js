a69ea07fd0ff830dd84589cc1d57de9321c55895
unknown <ianchen6501@gmail.com>
2020-07-17 23:08:46 +0800

    hw3

---
+++ homeworks/week4/hw3.js
@@ -0,0 +1,15 @@
+const request = require('request');
+const process = require('process');
+
+request(`https://restcountries.eu/rest/v2/name/' + ${process.argv[2]}`,
+  (error, response, body) => {
+    const json = JSON.parse(body);
+    if (response.statusCode >= 200 && response.statusCode < 300) {
+      console.log(json[0].name);
+      console.log(json[0].capital);
+      console.log(json[0].currencies[0].code);
+      console.log(json[0].callingCodes[0]);
+    } else {
+      console.log('找不到國家資訊');
+    }
+  });
